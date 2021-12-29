0d4263fb8833268abe5f20cadd962af6643a4b0a
unknown <bhumesh.ranjaneb598@gmail.com>
2021-12-29 23:30:37 +0530

    employee wage use case1 added to master

---
+++ src/com/bridge/javafundamentals/EmployeeWage.java
@@ -0,0 +1,23 @@
+package com.bridge.javafundamentals;
+
+import java.util.Random;
+import java.lang.Math;
+
+import static java.lang.Math.random;
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage Computation Program");
+        double random = Math.random() * 3;
+        System.out.println("random number generated is : " + random);
+         int result = (int) random%2;
+        System.out.println("result is :" + result);
+         if (result==1)
+             System.out.println("Employee is present");
+         else{
+             System.out.println("Employee is absent");
+         }
+    }
+}
+
