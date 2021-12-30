ef68f7f720043d209fde5bfd5f6c6807569d8e46
unknown <bhumesh.ranjaneb598@gmail.com>
2021-12-30 14:45:01 +0530

    UC3 added of part time employee & wage

---
+++ src/com/bridge/javafundamentals/Employee.java
@@ -0,0 +1,9 @@
+package com.bridge.javafundamentals;
+
+public class Employee {
+    String  jobType;
+    int numberOfHours;
+    int wage;
+    int totalWage;
+
+}
--- src/com/bridge/javafundamentals/EmployeeWage.java
+++ src/com/bridge/javafundamentals/EmployeeWage.java
@@ -1,12 +1,12 @@
 package com.bridge.javafundamentals;
 
-import java.util.Random;
-import java.lang.Math;
 
-import static java.lang.Math.random;
+import java.lang.Math;
 
 public class EmployeeWage {
 
+    public static final int EMPLOYEE_WAGE_PER_HOUR =20;
+    public static final  int FULL_DAY_HOUR = 8;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage Computation Program");
         double random = Math.random() * 2;
@@ -18,6 +18,19 @@ public class EmployeeWage {
          else{
              System.out.println("Employee is absent");
          }
+
+        int dailyWage = EMPLOYEE_WAGE_PER_HOUR * FULL_DAY_HOUR;
+                System.out.println("Daily employee wage is :" + dailyWage);
+                UC3();
+    }
+    static void UC3(){
+        Employee partTimeEmployee = new Employee();
+        partTimeEmployee.jobType = "part Time";
+        partTimeEmployee.wage = 15;
+        partTimeEmployee.numberOfHours = 8;
+        partTimeEmployee.totalWage =  partTimeEmployee.numberOfHours * partTimeEmployee.wage;
+        System.out.println("Total part time Wage: "+ partTimeEmployee.totalWage);
+
     }
 }
 
